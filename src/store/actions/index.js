b6388af556ff7c20aa1bfc0572765495f8498fdd
Your Name <you@example.com>
2020-08-11 18:08:28 +0530

    user authenticated using token

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,7 +2,12 @@ export {
     addIngredient, 
     removeIngredient, 
     initIngredient} from './burgerBuilder'
+    
 export { 
     purchaseBurger,
     purchaseInit,
 fetchOrder } from './order'
+
+export {
+    auth
+} from './auth'
